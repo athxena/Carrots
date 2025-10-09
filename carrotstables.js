e2781f19535e8c895d76c02d2db0b558ffdb1def
athena <ath@athenas-MacBook-Pro.local>
2025-10-09 14:04:40 -0700

    add stables lab 1
    ls
    sc
    
    sc

---
+++ carrotstables.js
@@ -0,0 +1,80 @@
+let horseName = "Carrots";
+let horseAge = 7;
+let isHorseInStable = true;
+let horseNickname = "Tomato";
+let favoriteTreat = "Pop tarts";
+let monthlyBoardingFee = 250;
+let monthsBoarded = 3;
+let lateFee = monthlyBoardingFee * 1.2;
+
+let visitorMessage = "Welcome! This is " + horseName;
+let latePaymentMessage =
+    horseName + "'s payment is late. Please pay: $" + lateFee;
+
+console.log(visitorMessage);
+console.log(latePaymentMessage);
+
+let horseOne = {
+    name: "Carrots",
+    nickname: "Tomato",
+    favoriteTreat: "Pop Tarts",
+    age: 7,
+    monthlyBoardingFee: 250,
+    isHorseInStable: true,
+    color: "Brown",
+    mane: "Long",
+};
+
+let horseTwo = {
+    name: "Lolipop",
+    nickname: "Poppy",
+    favoriteTreat: "Lettuce",
+    age: 67,
+    monthlyBoardingFee: 250,
+    isHorseInStable: true,
+    color: "Pink",
+    mane: "Short",
+};
+
+let horseThree = {
+    name: "Sword",
+    nickname: "ShaShing",
+    favoriteTreat: "Pizza",
+    age: 25,
+    monthlyBoardingFee: 250,
+    isHorseInStable: true,
+    color: "Black",
+    mane: "Long",
+};
+
+let horses = [horseOne, horseTwo, horseThree];
+
+let horseFour = {
+    name: "Fluffy",
+    nickname: "Pinky",
+    favoriteTreat: "Cotton Candy",
+    age: 3,
+    monthlyBoardingFee: 250,
+    isHorseInStable: true,
+    color: "Rainbow",
+    mane: "Fluffy",
+};
+
+horses.push(horseFour);
+
+horseOne.isHungry = false;
+horseTwo.isHungry = true;
+horseThree.isHungry = false;
+horseFour.isHungry = true;
+
+let horseOneHungryMessage = horseOne.name + " is hungry: " + horseOne.isHungry;
+let horseTwoHungryMessage = horseTwo.name + " is hungry: " + horseTwo.isHungry;
+let horseThreeHungryMessage =
+    horseThree.name + " is hungry: " + horseThree.isHungry;
+let horseFourHungryMessage =
+    horseFour.name + " is hungry: " + horseFour.isHungry;
+
+console.log(horseOneHungryMessage);
+console.log(horseTwoHungryMessage);
+console.log(horseThreeHungryMessage);
+console.log(horseFourHungryMessage);
